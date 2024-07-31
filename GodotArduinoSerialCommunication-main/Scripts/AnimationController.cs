991f3a9885f1267c418e25248fd51a31b80b8ad6
JacobM311 <jacobmarshall9608@yahoo.com>
2024-07-31 17:01:58 -0500

    updates scenes and added animation controller with title card animation

---
+++ GodotArduinoSerialCommunication-main/Scripts/AnimationController.cs
@@ -0,0 +1,38 @@
+using Godot;
+using System.Collections.Generic;
+using System.Linq;
+
+[GlobalClass]
+
+public partial class AnimationController : AnimationPlayer
+{
+	[Export] SlotController.SlotController slotController;
+
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+		slotController.SerialDataReceiver += DataReceived;
+		Play("TitleCardFade");
+	}
+
+	void DataReceived(string data)
+	{
+		List<string> values = data.Split(",").ToList();
+		bool allSame = values.All(str => str == values[0]);
+		
+		if (allSame)
+		{
+			Play("WinAimation");
+		}
+		else
+		{
+			GD.Print("loseanim");
+			Play("LoseAnimation");
+		}
+	}
+
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(double delta)
+	{
+	}
+}
